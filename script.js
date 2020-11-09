5bd6fa7fe7e5c9e5eb91ba9ea1def90031197e9f
Anderson Madeira <andersonmadeiracs@gmail.com>
2020-11-08 21:10:59 -0300

    Minute and second hands, clock ticking...

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+const ANGLES_PER_SECOND = 360 / 60
 const ANGLES_PER_MINUTE = 360 / 60
-const minutesBar = document.querySelector('.minutes-bar')
+const secondsHand = document.querySelector('.seconds-hand')
+const minutesHand = document.querySelector('.minutes-hand')
 
-let timer = 0
-let angleRad = 90
-let seconds = 0
+let tickId = 0
+let secondsHandAngle = 90
+let minutesHandAngle = 90
+let secondsElapsed = 0
+let minutesElapsed = 0
 let lastTimestamp = 0
 
 const tick = (timestamp) => {
     if (!lastTimestamp || timestamp - lastTimestamp >= 1000) {
         lastTimestamp = timestamp
-        angleRad += ANGLES_PER_MINUTE
-        seconds += 1
-        console.log(`${seconds}s`)
-        minutesBar.style.transform = `rotate(${angleRad}deg)`
+        secondsHandAngle += ANGLES_PER_SECOND
+        secondsElapsed += 1
+        console.log(`${secondsElapsed}s`)
+        secondsHand.style.transform = `rotate(${secondsHandAngle}deg)`
+
+        if (secondsElapsed === 60) {
+            secondsElapsed = 0
+            minutesHandAngle += ANGLES_PER_MINUTE
+            minutesElapsed += 1
+            console.log(`${minutesElapsed}m`)
+            minutesHand.style.transform = `rotate(${minutesHandAngle}deg)`
+        }
     }
 
-    requestAnimationFrame(tick)
+    tickId = requestAnimationFrame(tick)
 }
 
-requestAnimationFrame(tick)
+tickId = requestAnimationFrame(tick)
