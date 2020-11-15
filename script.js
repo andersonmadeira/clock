d63957750ecdaa7d83e23039869d6aef936a8f33
saurabh <dev.saurabh456@gmail.com>
2020-11-16 00:16:43 +0530

    transpiled script.js es6 code es5 using babel

--- script.js
+++ script.js
@@ -1,114 +1,107 @@
-const ANGLES_PER_SECOND = 360 / 60
-const ANGLES_PER_MINUTE = 360 / 60
-const ANGLES_PER_HOUR = 360 / 12
-const secondsHand = document.querySelector('.clock .seconds-hand')
-const minutesHand = document.querySelector('.clock .minutes-hand')
-const hoursHand = document.querySelector('.clock .hours-hand')
-const clockFace = document.querySelector('.clock .face')
-const timeLabel = document.querySelector('.clock .face .time-label')
+var ANGLES_PER_SECOND = 360 / 60;
+var ANGLES_PER_MINUTE = 360 / 60;
+var ANGLES_PER_HOUR = 360 / 12;
+var secondsHand = document.querySelector('.clock .seconds-hand');
+var minutesHand = document.querySelector('.clock .minutes-hand');
+var hoursHand = document.querySelector('.clock .hours-hand');
+var clockFace = document.querySelector('.clock .face');
+var timeLabel = document.querySelector('.clock .face .time-label');
+var tickId = 0;
+var secondsElapsed = 0;
+var minutesElapsed = 0;
+var hoursElapsed = 0;
+var secondsHandAngle = 90;
+var minutesHandAngle = 90;
+var hoursHandAngle = 90;
+var lastTimestamp = 0;
 
-let tickId = 0
-let secondsElapsed = 0
-let minutesElapsed = 0
-let hoursElapsed = 0
-let secondsHandAngle = 90
-let minutesHandAngle = 90
-let hoursHandAngle = 90
+var setTime = function setTime() {
+  var now = new Date();
+  secondsElapsed = now.getSeconds();
+  minutesElapsed = now.getMinutes();
+  hoursElapsed = now.getHours();
 
-let lastTimestamp = 0
+  if (hoursElapsed >= 12) {
+    hoursElapsed -= 12;
+  }
 
-const setTime = () => {
-    const now = new Date()
+  secondsHandAngle = 90 + secondsElapsed * ANGLES_PER_SECOND;
+  minutesHandAngle = 90 + minutesElapsed * ANGLES_PER_MINUTE;
+  hoursHandAngle = 90 + hoursElapsed * ANGLES_PER_HOUR;
+};
 
-    secondsElapsed = now.getSeconds()
-    minutesElapsed = now.getMinutes()
-    hoursElapsed = now.getHours() 
+var placeClockHands = function placeClockHands() {
+  secondsHand.style.transform = "rotate(".concat(secondsHandAngle, "deg)");
+  minutesHand.style.transform = "rotate(".concat(minutesHandAngle, "deg)");
+  hoursHand.style.transform = "rotate(".concat(hoursHandAngle, "deg)");
+};
 
-    if (hoursElapsed >= 12) {
-        hoursElapsed -= 12
+var setupClockFace = function setupClockFace() {
+  var angle = 90;
+
+  for (var pos = 0; pos <= 29; pos++) {
+    var bar = document.createElement('div');
+    bar.classList.add('face-bar');
+
+    if (pos % 5 === 0) {
+      bar.classList.add('face-bar--big');
     }
 
-    secondsHandAngle = 90 + secondsElapsed * ANGLES_PER_SECOND
-    minutesHandAngle = 90 + minutesElapsed * ANGLES_PER_MINUTE
-    hoursHandAngle = 90 + hoursElapsed * ANGLES_PER_HOUR
-}
+    bar.style.transform = "rotate(".concat(angle, "deg)");
+    clockFace.append(bar);
+    angle += ANGLES_PER_SECOND;
+  }
+};
 
-const placeClockHands = () => {
-    secondsHand.style.transform = `rotate(${secondsHandAngle}deg)`
-    minutesHand.style.transform = `rotate(${minutesHandAngle}deg)`
-    hoursHand.style.transform = `rotate(${hoursHandAngle}deg)`
-}
+var getCurrentTimeFormatted = function getCurrentTimeFormatted() {
+  return new Date().toLocaleTimeString();
+};
 
-const setupClockFace = () => {
-    let angle = 90
+var tick = function tick(timestamp) {
+  if (!lastTimestamp || timestamp - lastTimestamp >= 1000) {
+    lastTimestamp = timestamp;
+    secondsHandAngle += ANGLES_PER_SECOND;
+    secondsElapsed += 1;
+    secondsHand.style.transform = "rotate(".concat(secondsHandAngle, "deg)");
 
-    for (let pos = 0; pos <= 29; pos++) {
-        const bar = document.createElement('div')
-        
-        bar.classList.add('face-bar')
+    if (secondsElapsed === 60) {
+      secondsElapsed = 0;
+      minutesHandAngle += ANGLES_PER_MINUTE;
+      minutesElapsed += 1;
+      minutesHand.style.transform = "rotate(".concat(minutesHandAngle, "deg)");
 
-        if (pos % 5 === 0) {
-            bar.classList.add('face-bar--big')
+      if (minutesElapsed === 60) {
+        minutesElapsed = 0;
+        hoursHandAngle += ANGLES_PER_HOUR;
+        hoursElapsed += 1;
+        hoursHand.style.transform = "rotate(".concat(hoursHandAngle, "deg)");
+
+        if (hoursElapsed === 12) {
+          hoursElapsed = 0;
+          hoursHandAngle = 90;
+          console.log("reseting clock");
         }
-
-        bar.style.transform = `rotate(${angle}deg)`
-        clockFace.append(bar)
-
-        angle += ANGLES_PER_SECOND
-    }
-}
-
-const getCurrentTimeFormatted = () => new Date().toLocaleTimeString()
-
-const tick = (timestamp) => {
-    if (!lastTimestamp || timestamp - lastTimestamp >= 1000) {
-        lastTimestamp = timestamp
-        secondsHandAngle += ANGLES_PER_SECOND
-        secondsElapsed += 1
-
-        secondsHand.style.transform = `rotate(${secondsHandAngle}deg)`
-
-        if (secondsElapsed === 60) {
-            secondsElapsed = 0
-            minutesHandAngle += ANGLES_PER_MINUTE
-            minutesElapsed += 1
-            
-            minutesHand.style.transform = `rotate(${minutesHandAngle}deg)`
-
-            if (minutesElapsed === 60) {
-                minutesElapsed = 0
-                hoursHandAngle += ANGLES_PER_HOUR
-                hoursElapsed += 1
-
-                hoursHand.style.transform = `rotate(${hoursHandAngle}deg)`
-
-                if (hoursElapsed === 12) {
-                    hoursElapsed = 0
-                    hoursHandAngle = 90
-                    console.log(`reseting clock`)
-                }
-            }
-        }
-
-        timeLabel.textContent = getCurrentTimeFormatted()
+      }
     }
 
-    tickId = requestAnimationFrame(tick)
-}
+    timeLabel.textContent = getCurrentTimeFormatted();
+  }
 
-const updateClock = () => {
-    setTime()
-}
+  tickId = requestAnimationFrame(tick);
+};
 
+var updateClock = function updateClock() {
+  setTime();
+};
 
-const init = () => {
-    updateClock()
-    setupClockFace()
-    placeClockHands()
+var init = function init() {
+  updateClock();
+  setupClockFace();
+  placeClockHands();
+  window.addEventListener('focus', function () {
+    return updateClock();
+  });
+  tickId = requestAnimationFrame(tick);
+};
 
-    window.addEventListener('focus', () => updateClock())
-
-    tickId = requestAnimationFrame(tick)
-}
-
-init()
+init();
